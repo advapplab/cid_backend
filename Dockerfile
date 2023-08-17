3fff78a30b379e6fba26a64e767e7a528f4fc3ed
Owen HT Lu <cfleu198@gmail.com>
2023-08-17 23:40:06 +0800

    post image

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN pip3 install cmake
 RUN pip3 install -r requirements.txt 
 ENTRYPOINT python3 main.py
 
-EXPOSE 8080
+EXPOSE 8000
--- main.py
+++ main.py
@@ -148,4 +148,4 @@ def root():
 #     # return send_file('gen_image.png', mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
