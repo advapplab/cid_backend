c993153e757cc8a2cc9c1babf6e6300a9143ea78
Owen HT Lu <cfleu198@gmail.com>
2023-08-17 19:51:33 +0800

    fix

--- main.py
+++ main.py
@@ -93,48 +93,51 @@ def gen_image():
         return send_file('gen_image.png', mimetype='image/png')
 
 
+@app.route('/')
+def root():
+    return send_from_directory(app.static_folder, 'index.html')
 
-@app.route("/test", methods=['GET'])
-def home():
+# @app.route("/test", methods=['GET'])
+# def home():
 
-    # # choose a random 
-    # option1 = random.choice(options_character)
-    # option2 = random.choice(options_location)
+#     # # choose a random 
+#     # option1 = random.choice(options_character)
+#     # option2 = random.choice(options_location)
 
-    # # replace string
-    # prompt = "in location, a person, alone, facing the camera, solo, skin detail, face detail, Taiwanese, raw photo ,8K HDR, hyper-realistic, half body shot, hyper detailed, cinematic lighting"
-    # prompt = prompt.replace("a person", option1).replace("in location", option2)
-    # neg_prompt = "nsfw, nude, censored, ((duplication)), more than one person, text, watermark, blurry background, naked, half naked, topless, wearing underwear, showing thighs, showing chest, deformed iris, deformed pupils, out of frame, cropped, not wearing pants,semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime, mutated hands and fingers:1.4), (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, disconnected limbs, mutation, mutated, ugly, disgusting, amputation, worst quality, normal quality, low quality, low res, blurry, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch ,duplicate, ugly, monochrome, horror, geometry, mutation, disgusting, bad anatomy, bad hands, three hands, three legs, bad arms, missing legs, missing arms, poorly drawn face, bad face, fused face, cloned face, worst face, three crus, extra crus, fused crus, worst feet, three feet, fused feet, fused thigh, three thigh, fused thigh, extra thigh, worst thigh, missing fingers, extra fingers, ugly fingers, long fingers, horn, extra eyes, huge eyes, 2girl, amputation, disconnected limbs, cartoon, cg, 3d, unreal, animate"
-
-
-    # data = {'prompt': prompt,
-    #         "negative_prompt": neg_prompt,
-    #         "sampler_name": "DPM++ 2M Karras",
-    #         'width': 32,
-    #         'height': 32}
-
-    # response = submit_post(sd_host, data)
-    # image_base64 = response.json()['images'][0]
-
-    # with open('gen_image.png', "wb") as image_file:
-    #     image_file.write(base64.b64decode(image_base64))
+#     # # replace string
+#     # prompt = "in location, a person, alone, facing the camera, solo, skin detail, face detail, Taiwanese, raw photo ,8K HDR, hyper-realistic, half body shot, hyper detailed, cinematic lighting"
+#     # prompt = prompt.replace("a person", option1).replace("in location", option2)
+#     # neg_prompt = "nsfw, nude, censored, ((duplication)), more than one person, text, watermark, blurry background, naked, half naked, topless, wearing underwear, showing thighs, showing chest, deformed iris, deformed pupils, out of frame, cropped, not wearing pants,semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime, mutated hands and fingers:1.4), (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, disconnected limbs, mutation, mutated, ugly, disgusting, amputation, worst quality, normal quality, low quality, low res, blurry, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch ,duplicate, ugly, monochrome, horror, geometry, mutation, disgusting, bad anatomy, bad hands, three hands, three legs, bad arms, missing legs, missing arms, poorly drawn face, bad face, fused face, cloned face, worst face, three crus, extra crus, fused crus, worst feet, three feet, fused feet, fused thigh, three thigh, fused thigh, extra thigh, worst thigh, missing fingers, extra fingers, ugly fingers, long fingers, horn, extra eyes, huge eyes, 2girl, amputation, disconnected limbs, cartoon, cg, 3d, unreal, animate"
 
 
+#     # data = {'prompt': prompt,
+#     #         "negative_prompt": neg_prompt,
+#     #         "sampler_name": "DPM++ 2M Karras",
+#     #         'width': 32,
+#     #         'height': 32}
+
+#     # response = submit_post(sd_host, data)
+#     # image_base64 = response.json()['images'][0]
+
+#     # with open('gen_image.png', "wb") as image_file:
+#     #     image_file.write(base64.b64decode(image_base64))
 
 
 
 
-    # return_dict = dict()
-    # return_dict['image'] = image_base64
-    # return_dict['qr'] = qr_img_base64  
-    # # print(return_dict)
-
-    # # return_json = jsonify(return_dict)
-    # # print(return_json)
 
 
-    return 'hello world'
-    # return send_file('gen_image.png', mimetype='image/png')
+#     # return_dict = dict()
+#     # return_dict['image'] = image_base64
+#     # return_dict['qr'] = qr_img_base64  
+#     # # print(return_dict)
+
+#     # # return_json = jsonify(return_dict)
+#     # # print(return_json)
+
+
+#     return 'hello world'
+#     # return send_file('gen_image.png', mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
