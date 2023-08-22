553c77718d5c505ec596d936c472130d8bb633ca
Owen HT Lu <cfleu198@gmail.com>
2023-08-22 20:58:47 +0800

    test

--- html/script.js
+++ html/script.js
@@ -1,18 +1,17 @@
 
 
 
-(function() {
 
 
 
-    resetbutton = document.getElementById('resetbutton');
 
-    resetbutton.addEventListener('click', function(ev) {
-        location.reload();
-        ev.preventDefault();
-    }, false);
+resetbutton = document.getElementById('resetbutton');
 
-    // document.getElementById('reset').addEventListener('click', function() {
-    //     location.reload();
-    // });
-}
+resetbutton.addEventListener('click', function(ev) {
+    location.reload();
+    ev.preventDefault();
+}, false);
+
+// document.getElementById('reset').addEventListener('click', function() {
+//     location.reload();
+// });
