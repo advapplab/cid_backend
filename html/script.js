0cf8b43466bb281155dddd5e3c5728505af3c339
Owen HT Lu <cfleu198@gmail.com>
2023-08-22 21:11:44 +0800

    test

--- html/script.js
+++ html/script.js
@@ -14,3 +14,32 @@ console.log(currentURL);
 document.getElementById('reset').addEventListener('click', function() {
     location.reload();
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('webcam');
+    const canvas = document.getElementById('canvas');
+    const captureButton = document.getElementById('capture');
+    const ctx = canvas.getContext('2d');
+
+    // Adjust these constraints to get the desired resolution
+    const constraints = {
+        video: {
+            width: 1280,
+            height: 720,
+            facingMode: 'user'
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+            video.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Error accessing webcam: ", error);
+        });
+
+    captureButton.addEventListener('click', function() {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    });
+});
