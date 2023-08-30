5a00d3ec43067abf8811c79b5149fb89e64a8328
Owen HT Lu <cfleu198@gmail.com>
2023-08-31 01:29:54 +0800

    fix

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ mdb_pass = os.getenv('MONGODB_PASSWORD')
 mdb_host = os.getenv('MONGODB_HOST')
 mdb_dbs = os.getenv('MONGODB_DATABASE')
 
-sd_host = 'http://127.0.0.1:3000'
+sd_host = 'http://192.168.0.106:3000'
 sr_host = 'http://140.119.112.78:8828'
 
 app = Flask(__name__, static_folder="/")
